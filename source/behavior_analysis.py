dd3aa8fbf7ec8777969f247fee8b6d3b1a24a721
Daniel Bliss <dbliss@berkeley.edu>
2017-10-21 11:53:38 -0700

    Add Clifford and DoG code to ba.

--- source/behavior_analysis.py
+++ source/behavior_analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import io as sio
 import matlab
 import matlab.engine
+from scipy.optimize import least_squares
 
 
 def get_subject_data(subject, sessions, task, keys, indices,
@@ -185,3 +186,170 @@ def get_sys_error(df):
     df['global_sys_error'] = sys_error
     df['global_resid_error'] = df['errors'] - df['global_sys_error']
     return df
+
+
+def clifford(x, c, s):
+    """Compute Clifford's tilt model function.
+
+    Parameters
+    ----------
+    x : numpy.array
+      Location of each trial's previous target, relative to the current
+      target in radians.
+
+    c : number
+      c parameter.
+
+    s : number
+      s parameter.
+
+    Returns
+    -------
+    numpy.array
+      The value of Clifford's function for the submitted values of x.
+    
+    """
+    theta_ad = np.arcsin((np.sin(x)) / np.sqrt(((s * np.cos(x) - c)) ** 2 +
+                                               (np.sin(x)) ** 2))
+    test = s * np.cos(x) - c < 0
+    theta_ad[test] = np.pi - theta_ad[test]
+    return np.mod(theta_ad - x + np.pi, 2 * np.pi) - np.pi
+
+
+def fit_clifford(y, x):
+
+    """Fit Clifford's tilt model to observed errors.
+
+    Parameters
+    ----------
+    y : numpy.array
+      Residual error (in radians).
+
+    x : numpy.array
+      Location of previous stimulus relative to the current one (in
+      radians).
+    
+    Returns
+    -------
+    float
+      Computed c for the Clifford fit.
+
+    float
+      Computed s for the Clifford fit.
+
+    float
+      Computed sign for the Clifford fit.
+
+    float
+      Cost for the fit.
+
+    """
+
+    def _solver(params):
+        c, s, m = params
+        m = np.sign(m)
+        return y - m * clifford(x, c, s)
+
+    min_c = 0.0
+    max_c = 1.0
+    
+    min_s = 0.0
+    max_s = 1.0
+
+    min_m = -1.0
+    max_m = 1.0
+    
+    min_cost = np.inf
+    for _ in range(200):
+        params_0 = [np.random.rand() * (max_c - min_c) + min_c,
+                    np.random.rand() * (max_s - min_s) + min_s,
+                    np.random.rand() * (max_m - min_m) + min_m]
+        try:
+            result = least_squares(_solver, params_0,
+                                   bounds=([min_c, min_s, min_m],
+                                           [max_c, max_s, max_m]))
+        except ValueError:
+            continue
+        if result['cost'] < min_cost:
+            best_params, min_cost = result['x'], result['cost']
+    try:
+        return best_params[0], best_params[1], np.sign(best_params[2]), min_cost
+    except UnboundLocalError:
+        return np.nan, np.nan, np.nan, min_cost
+
+    
+def dog(x, a, w):
+    c = np.sqrt(2) / np.exp(-0.5)
+    return x * a * w * c * np.exp(-(w * x) ** 2)
+
+
+def fit_dog(y, x):
+
+    def _solver(params):
+        a, w = params
+        return y - dog(x, a, w)
+
+    min_a = -np.pi
+    max_a = np.pi
+
+    min_w = 0.4
+    max_w = 4.0
+
+    min_cost = np.inf
+    for _ in range(200):
+        params_0 = [np.random.rand() * (max_a - min_a) + min_a,
+                    np.random.rand() * (max_w - min_w) + min_w]
+        try:
+            result = least_squares(_solver, params_0,
+                                   bounds=([min_a, min_w],
+                                           [max_a, max_w]))
+        except ValueError:
+            continue
+        if result['cost'] < min_cost:
+            best_params, min_cost = result['x'], result['cost']
+    try:
+        return best_params[0], best_params[1], min_cost
+    except UnboundLocalError:
+        return np.nan, np.nan, min_cost
+
+    
+def print_fit_goodness(data_frames):
+
+    n_subs = len(data_frames)
+    aic_c_values = np.empty((2, n_subs))
+    for i, df in enumerate(data_frames):
+
+        d_stim = np.array(df['d_stim'])
+        error = np.array(df['global_resid_error'])
+
+        ind = ~np.isnan(d_stim)
+        d_stim_rad = np.deg2rad(d_stim[ind])
+        error_rad = np.deg2rad(error[ind])
+            
+        assert len(d_stim_rad) == len(error_rad)
+        total_n = len(d_stim_rad)
+
+        # Get Clifford fit.
+        cliff_params = fit_clifford(error_rad, d_stim_rad)
+
+        # Compute AICc.
+        rss = ((error_rad -
+                cliff_params[2] * clifford(d_stim_rad, cliff_params[0],
+                                           cliff_params[1])) ** 2).sum()
+        k = len(cliff_params[:-1])
+        aic = 2 * k + total_n * np.log(rss)
+        aic_c_values[0, i] = aic + 2 * k * (k + 1) / (total_n - k - 1)
+
+        # Get DoG fit.
+        dog_params = fit_dog(error_rad, d_stim_rad)
+
+        # Compute AICc.
+        rss = ((error_rad - dog(d_stim_rad, dog_params[0],
+                                dog_params[1])) ** 2).sum()
+        k = len(dog_params[:-1])
+        aic = 2 * k + total_n * np.log(rss)
+        aic_c_values[1, i] = aic + 2 * k * (k + 1) / (total_n - k - 1)
+
+    print 'Clifford - DoG:'
+    print (aic_c_values[0, :] - aic_c_values[1, :]).mean()
+    print (aic_c_values[0, :] - aic_c_values[1, :]).std() / np.sqrt(n_subs)
